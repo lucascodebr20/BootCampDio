51e811bd05c5c4f67faf1f0f0b4343ce4b6c828c
Lucas Campos <lucascode@gmail.com>
2024-05-22 10:45:08 -0300

    atualização 22/05/2024

---
+++ Estudo/Trilha-Basico-Java/src/BomDia.java
@@ -0,0 +1,12 @@
+public class BomDia {
+
+        public static void main ( String [] args) {
+
+        String comeco = "Bom";
+        String fim = "Dia";
+        String juntar = comeco + " " + fim;
+    
+        System.out.println(juntar);
+                
+    };
+};
---
+++ Estudo/Trilha-Basico-Java/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+
+        int idadePessoa = 31;
+        int anoAtual = 2024;
+        int descobrirIdade = anoAtual - idadePessoa;
+    
+        System.out.println("Seu ano de nascimento é " + descobrirIdade);
+                
+    };
+
+
+    
+};
