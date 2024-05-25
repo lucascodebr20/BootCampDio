1797252c0d192351755664fc5eea817536d91949
Lucas Campos <lucascode@gmail.com>
2024-05-25 01:56:31 -0300

    Exercicio ValidarLogin

--- Estudo/Controle de Fluxo/src/ValidarLogin.java
+++ Estudo/Controle de Fluxo/src/ValidarLogin.java
@@ -1,11 +1,27 @@
-
+import java.util.Scanner;
 
 public class ValidarLogin {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         
+            try (Scanner scanner = new Scanner(System.in)) {
+            boolean validador = false;
 
-        
+            while (validador == true) {
+
+                System.out.println("Digite o seu login:");
+                String login = scanner.next();
+                System.out.println("Digite a sua senha:");
+                String senha = scanner.next();
+
+                if ("admin".equals(login) && "senha123".equals(senha)) { 
+                    validador = true;
+                    System.out.println("Login Feito com Sucesso!");
+                }
+
+                System.out.println("Login ou Senha estão incorretos!");
+            }
+        }
     }
 
 }
