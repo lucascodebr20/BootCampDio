18f0a2edf105c368148293af3bb6f9e1c6eb6ba2
Lucas Campos <lucascode@gmail.com>
2024-05-25 11:47:42 -0300

    Exercicio Corrigido

--- Estudo/Controle de Fluxo/src/ForSimples.java
+++ Estudo/Controle de Fluxo/src/ForSimples.java
@@ -1,13 +1,27 @@
 
+import java.util.Scanner;
+
 
 public class ForSimples {
 
     public static void main(String[] args) {
 
         
+        try (Scanner scanner = new Scanner(System.in)) {
 
+            System.out.println("Digite um numero:");
+            int numeroCadastrado = scanner.nextInt();
+            System.out.println("");
 
+            for (int i = 1; i <= numeroCadastrado; numeroCadastrado--) {
 
+                    if (numeroCadastrado % 2 == 0) {
+                        
+                    } else {
+                        System.out.println("O " + numeroCadastrado + " é impar !");
+                    }
+             }   
+        }
     }
-    
+
 }
